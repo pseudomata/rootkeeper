56d882b898bd9d9b0b771b56c5e17a694c88ebe5
Pseudomata <pseudomata@skiff.com>
2023-08-04 10:59:46 -0400

    dev: a bit more progress on the commands and parsing for cli stuff

--- src/main.zig
+++ src/main.zig
@@ -10,26 +10,36 @@ const usage =
     \\
     \\Commands:
     \\
-    \\  help             Prints this help text and exit
-    \\  init             Initialize a new repository with rootkeeper
-    \\  version          Print version number and exit
+    \\  help                 Prints this help text and exit
+    \\  init                 Initialize a new repository with rootkeeper
+    \\  update               Update configuration links and git ignores
+    \\  version              Print version number and exit
     \\
     \\General Options:
     \\
-    \\  -h, --help       Print command-specific usage
+    \\  -c, --config <path>  Path to configuration if not default (default: 'Rootfile')
+    \\  -h, --help           Print command-specific usage
+    \\  -v, --verbose        Log what rootkeeper is doing verbosely
     \\
 ;
+
+const Command = enum {
+    help,
+    init,
+    update,
+    version,
+};
+
+const Options = struct {
+    config: []u8 = "Rootfile",
+    help: bool = false,
+    verbose: bool = false,
+};
 
 pub fn fatal(comptime format: []const u8, args: anytype) noreturn {
     std.log.err(format, args);
     process.exit(1);
 }
-
-const Command = enum {
-    help,
-    init,
-    version,
-};
 
 pub fn printHelp() noreturn {
     print(usage, .{});
@@ -53,9 +63,53 @@ pub fn parseCommand(cmd: []const u8) Command {
     }
 }
 
-// pub fn parse_command_args() {}
+pub fn parseOptions(args: [][]u8) Options {
+    var opts = Options{};
 
-// pub fn parse_flags() {}
+    for (args) |arg| {
+        if (mem.eql(u8, "-h", arg) or mem.eql(u8, "--help", arg)) {
+            opts.help = true;
+        } else if (mem.eql(u8, "-v", arg) or mem.eql(u8, "--verbose", arg)) {
+            opts.verbose = true;
+        } else if (mem.eql(u8, "-c", arg) or mem.eql(u8, "--config", arg)) {
+            // if (index + 1 >= args.len) {
+            //     fatal("Missing path to configuration file.", .{});
+            // }
+            const file_path = "get_this_from_arg+1";
+            opts.config = file_path;
+        } else {
+            continue;
+        }
+    }
+    return opts;
+}
+
+pub fn initializeRepo(opts: Options) noreturn {
+    _ = opts;
+    print("initialize repo", .{});
+}
+
+pub fn updateRepo(opts: Options) noreturn {
+    _ = opts;
+    print("update repo", .{});
+}
+
+pub fn executeCommand(cmd: Command, opts: Options) !void {
+    switch (cmd) {
+        Command.help => {
+            printHelp();
+        },
+        Command.init => {
+            initializeRepo(opts);
+        },
+        Command.update => {
+            updateRepo(opts);
+        },
+        Command.version => {
+            printVersion();
+        },
+    }
+}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -68,24 +122,7 @@ pub fn main() !void {
     }
 
     const cmd = parseCommand(args[1]);
-    // const cmd_args = args[2..];
+    const opts = parseOptions(args[2..]);
 
-    switch (cmd) {
-        Command.help => {
-            printHelp();
-        },
-        Command.init => {
-            print("We are in: {}", .{cmd});
-        },
-        Command.version => {
-            printVersion();
-        },
-    }
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    executeCommand(cmd, opts);
 }
